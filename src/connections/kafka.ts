91c0d8edea8188f95c58880983ebe69d4bfa5139
Mohamad98saayed <msaayed98.io@gmail.com>
2024-06-15 11:17:11 +0300

    adding more conf fir kafka

--- src/connections/kafka.ts
+++ src/connections/kafka.ts
@@ -1,9 +1,13 @@
-import { Kafka } from "kafkajs";
+import dotenv from "dotenv"
+import { Kafka } from "kafkajs"
+
+// DOTENV CONFIGURATION
+dotenv.config();
 
 const kafkaServer = new Kafka({
-     clientId: "fleet-app",
-     brokers: ["localhost:9092"],
+     clientId: process.env.CLIENT_ID,
+     brokers: [process.env.BROKER!],
 })
 
 export const kafkaProducer = kafkaServer.producer();
-export const kafkaConsumer = kafkaServer.consumer({ groupId: "test-group" });
+export const kafkaConsumer = kafkaServer.consumer({ groupId: process.env.GROUP_ID! });
